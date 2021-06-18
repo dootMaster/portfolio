8fa13d17b797a07feb78350825970bb629ea36e6
Leslie Ngo <ngo.leslie@gmail.com>
2021-06-17 18:53:45 -0700

    edited more About section text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,9 @@ const About = () => {
   return (
     <div id="about-me">
     <div>About</div>
-      <p>Software engineer with a demonstrated mindset for clean, modular code in order to achieve scalability and optimization. I work with Javascript, React.js, Express.js, CSS3, Postgres, MySQL, MondoDB, AWS, and NGINX to create full-stack applications. I'm currently developing a web application that helps guitarists improve their fretboard navigation abilities. I started programming by teaching myself C# for game development, and eventually migrated to Godot and its native language, GDscript.</p>
+      <p>Software engineer with a demonstrated mindset for clean, modular code in order to achieve scalability and optimization.
+        I work with JavaScript, React.js, Express.js, CSS3, PostgreSQL, MySQL, MongoDB, AWS, and NGINX to create full-stack applications.
+        I'm currently developing a web application that helps guitarists improve their fretboard navigation abilities.</p>
   </div>
   )
 }
