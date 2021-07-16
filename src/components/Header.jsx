98839877e78977cd4c25cc4719ecba0fb0bcc38e
Leslie Ngo <ngo.leslie@gmail.com>
2021-07-15 20:25:49 -0700

    made mobile CSS breakpoint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div id="header">
+      <img id="me-image" src="me-nice.jpg" alt="picture of good looking guy"></img>
       <div id="my-name">l e s l i e N g o</div>
       <div className="my-titles">s o f t w a r e E n g i n e e r .</div>
     </div>
