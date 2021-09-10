df6b7278d0492eee68e3434415fc8594a6a32dc3
Leslie Ngo <ngo.leslie@gmail.com>
2021-09-10 01:22:37 -0700

    complete reset of CSStargetting minimalist web design goals

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,15 @@
 import React, { useState } from 'react';
-import Header from './components/Header.jsx';
-import About from './components/About.jsx';
-import SkillLogos from './components/SkillLogos.jsx';
+import Main from './components/Main.jsx';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="contents">
-        <div className="about-and-skills-container">
-          <About />
-          <SkillLogos />
-        </div>
-
-        <div id="projects-container">
-
-          <div className="project" id="project-key-frame-guitar">
-            <div className="project-header">
-              <h3 className="project-title">Key Frame Guitar</h3>
-                <div className="button-container">
-                  <button className="project-button">
-                    <p><a href="https://dootmaster.github.io/keyframeguitar/" target="_blank" rel="noopener noreferrer">Poke around the app.</a></p>
-                  </button>
-                  <button className="project-button">
-                    <p><a href="https://github.com/dootMaster/keyframeguitar" target="_blank" rel="noopener noreferrer">Poke around the code.</a></p>
-                  </button>
-                </div>
-            </div>
-              <div className="project-body">
-                <img className="project-image" src="main-active.png" alt="image of key frame guitar"></img>
-                <p className="project-description">A guitar navigation app I built inspired by key frames in animation.
-                  This app displays the fretboard map of two harmonies and their possible intersections based on user input.</p>
-              </div>
-          </div><br />
-
-          <div className="project" id="project-sdc">
-            <div className="project-header">
-              <h3 className="project-title">Atelier Product API</h3>
-              <button className="project-button">
-                <p><a href="https://github.com/maythovens-third/Products" target="_blank" rel="noopener noreferrer">Poke around the code.</a></p>
-              </button>
-            </div>
-            <div className="project-body">
-              <img className="project-image" src="4.png" alt="image of Atelier API tests"></img>
-              <p className="project-description">An API that provides data to a fashion products merchant site.
-              Three instances of Express.js servers were deployed behind an NGINX load balancer, and connected to an instance of PostgreSQL.
-              I wrote K6 tests that created up to 3000 virtual users that bombard the load balancer with requests in order to locate where optimizations can be made to the system.</p>
-            </div>
-          </div><br />
-
-          <div className="project" id="project-catwalk">
-            <div className="project-header">
-              <h3 className="project-title">project:Catwalk</h3>
-              <button className="project-button">
-                <p><a href="https://github.com/Weyland-Software-Group/fec/tree/main/client/src/components/Related" target="_blank" rel="noopener noreferrer">Poke around the code.</a></p>
-              </button>
-            </div>
-            <div className="project-body">
-              <iframe width="100%" height="auto"
-                src="https://youtube.com/embed/jaNWTNB9OsA">
-              </iframe>
-              <p className="project-description">An e-commerce site where I built the "Related Products" module. I built these carousels from scratch to enhance customizability.</p>
-            </div>
-          </div>
-        </div>
-
+    <div className="app-container">
+      <Navbar />
+      <div className="main-container">
+        <Main />
       </div>
-    </>
+    </div>
   );
 }
 
