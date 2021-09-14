5b1eea4dde1e278c632bf3fdeef5ffea8f047417
Leslie Ngo <ngo.leslie@gmail.com>
2021-09-13 23:43:53 -0700

    changed resume link from gdoc link to PDF download

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <ul className="navlist">
         <li><a href="https://github.com/dootMaster">github</a></li>
         <li><a href="https://www.linkedin.com/in/leslie-ngo/">linkedIn</a></li>
-        <li><a href="https://docs.google.com/document/d/1WPxfLVLeJL16crd7BkGZYUVbkhIF9b7G53HRJr5u1ks/edit?usp=sharing">resume</a></li>
+        <li><a href="/docs/Leslie Ngo Resume.pdf" download>resume</a></li>
       </ul>
     </div>
   )
