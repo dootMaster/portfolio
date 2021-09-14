b4a6e0e46e1c6c98c2871fec913168b660382a4f
Leslie Ngo <ngo.leslie@gmail.com>
2021-09-13 23:58:09 -0700

    changed download link text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <ul className="navlist">
         <li><a href="https://github.com/dootMaster">github</a></li>
         <li><a href="https://www.linkedin.com/in/leslie-ngo/">linkedIn</a></li>
-        <li><a href="/docs/Leslie Ngo Resume.pdf" download>download resume</a></li>
+        <li><a href="/docs/Leslie Ngo Resume.pdf" download>resume download</a></li>
       </ul>
     </div>
   )
