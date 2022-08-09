be81a7828b2a1b4237ad1ad5b76fd33b4436d50b
Leslie Ngo <ngo.leslie@gmail.com>
2022-08-08 18:06:23 -0700

    navBaf: comment out blog component
    currently not being used.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <li><a href="https://github.com/dootMaster">github</a></li>
         <li><a href="https://www.linkedin.com/in/leslie-ngo/">linkedIn</a></li>
         <li><a href="https://github.com/dootMaster/portfolio/raw/main/docs/Leslie-Ngo-Resume.pdf" download>resume download</a></li>
-        <li><Link to="/blog">blog</Link></li>
+//         <li><Link to="/blog">blog</Link></li>
       </ul>
     </div>
   )
